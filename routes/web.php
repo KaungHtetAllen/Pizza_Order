cbfae09ba3abb611f961e641a5fdf2eceec9f756
Kaung Htet <kaunghtet22122003@gmail.com>
2024-05-05 01:22:32 +0630

    Done register, login, logout process

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,20 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
+//login, register
+Route::redirect('/','loginPage');
+Route::get('loginPage',[AuthController::class,'loginPage'])->name('auth#loginPage');
+Route::get('registerPage',[AuthController::class,'registerPage'])->name('auth#registerPage');
+
+
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    //category
+    Route::group(['prefix'=>'category'],function(){
+        Route::get('list', [CategoryController::class,'list'])->name('category#list');
+    });
 });
 
-Route::get('/register', function(){
-    return view('register');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
